b573beebbd87047dfaf38881a08215a12fe88b55
Mahmoud Hefny <hefny4@gmail.com>
2022-08-13 13:41:45 +0200

    Commit Message

--- learningDjango/NEW/tryDjango/tryDjango/urls.py
+++ learningDjango/NEW/tryDjango/tryDjango/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import *
+#from products.views import 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('home/', home_view, name='home'),
+    path('contact/', contact_view),
 ]
