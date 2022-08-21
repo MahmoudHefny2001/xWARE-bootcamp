a101acf5995fdebd0dd4e32c0ee281de7a061b98
Mahmoud Hefny <hefny4@gmail.com>
2022-08-21 12:11:17 +0200

    Project Added

--- discussionBoard/accounts/urls.py
+++ discussionBoard/accounts/urls.py
@@ -8,5 +8,9 @@ from accounts import views
 urlpatterns = [
     path('signup/', views.signup, name = 'signup'),
     path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name = 'login')
+    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name = 'login'),
+    path('settings/changePassword/', auth_views.PasswordChangeView.as_view(template_name = 'changePassword.html'), name = 'Change Password'),
+    path('settings/changePassword/done/', auth_views.PasswordChangeDoneView.as_view(template_name = 'DoneChangingPassword.html'), name = 'DoneChangingPassword'),
+    
+
 ]
